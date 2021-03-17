c73b11c51069cd04bbd2e84d5b630d8339932823
DevGragy <eliasgarcia971@gmail.com>
2021-03-17 17:30:21 -0600

    Se puso comentario de prueba en app

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.MYSQL_URI);
 
 sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Its alive!!!");
-  })
-  .catch((err) => {
-    console.log("No se conecto :(", err);
-  });
+    .authenticate()
+    .then(() => {
+        console.log("Its alive!!!");
+    })
+    .catch((err) => {
+        console.log("No se conecto :(", err);
+    });
+
+//Hola mundo
+const hola = 10;
 const app = express();
 
 app.use(cors());
@@ -23,11 +26,11 @@ app.use(bodyParser.json());
 app.use("/v1", require("./routes"));
 
 app.use((req, res, next) => {
-  const err = new Error("Not Found");
-  err.status = 404;
-  next(err);
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
-  console.log("Server on port " + server.address().port);
+    console.log("Server on port " + server.address().port);
 });
